800f2791faf44dc187f9a6aded0436b6ed1ebaa5
hamaianhnhi <tuananhhcmus@gmail.com>
2014-10-16 11:20:59 +0700

    add setText(venture).get("emptyCart")

--- src/test/java/testsuite/Cart_Test.java
+++ src/test/java/testsuite/Cart_Test.java
@@ -4,6 +4,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import scenario.CartScenario;
 import scenario.WishListScenario;
 
 import static util.Helper.driver;
@@ -12,7 +13,7 @@ import static util.VentureText.setText;
 /**
  * Created by lazhcm10136 on 10/2/14.
  */
-public class Cart_Test extends WishListScenario {
+public class Cart_Test extends CartScenario {
 
     @BeforeMethod
     public void setUp() throws Exception {
@@ -35,25 +36,25 @@ public class Cart_Test extends WishListScenario {
         };
     }
 
-    @Test(dataProvider = "getVenturesDataToTest" )
+    @Test(dataProvider = "getVenturesDataToTest", enabled = false)
     public void test_CartAddProduct(String venture) throws Exception {
-        wishListNotLogin(venture, setText(venture).get("menuWiz"), setText(venture).get("wishList"), setText(venture).get("emptyWL"),
+        cartAddProduct(venture, setText(venture).get("menuWiz"), setText(venture).get("wishList"), setText(venture).get("emptyWL"),
                 setText(venture).get("categories"), setText(venture).get("filterWiz"),
-                setText(venture).get("prodWiz"), setText(venture).get("addWL"));
+                setText(venture).get("prodWiz"), setText(venture).get("addWL"),setText(venture).get("emptyCart"));
     }
 
-    @Test(dataProvider = "getVenturesDataToTest" )
+    @Test(dataProvider = "getVenturesDataToTest", enabled = false )
     public void test_CartDeleteProduct(String venture) throws Exception {
-        wishListNotLogin(venture, setText(venture).get("menuWiz"), setText(venture).get("wishList"), setText(venture).get("emptyWL"),
+        cartDeleteProduct(venture, setText(venture).get("menuWiz"), setText(venture).get("wishList"), setText(venture).get("emptyWL"),
                 setText(venture).get("categories"), setText(venture).get("filterWiz"),
-                setText(venture).get("prodWiz"), setText(venture).get("addWL"));
+                setText(venture).get("prodWiz"), setText(venture).get("addWL"),setText(venture).get("emptyCart") );
     }
 
     @Test(dataProvider = "getVenturesDataToTest" )
     public void test_CartChangeQuantity(String venture) throws Exception {
-        wishListNotLogin(venture, setText(venture).get("menuWiz"), setText(venture).get("wishList"), setText(venture).get("emptyWL"),
+        cartChangeQuantity(venture, setText(venture).get("menuWiz"), setText(venture).get("wishList"), setText(venture).get("emptyWL"),
                 setText(venture).get("categories"), setText(venture).get("filterWiz"),
-                setText(venture).get("prodWiz"), setText(venture).get("addWL"));
+                setText(venture).get("prodWiz"), setText(venture).get("addWL"), setText(venture).get("emptyCart"));
     }
 
 
