e095989e5d56c932611bb3ce424b5d3c418fd77d
hamaianhnhi <tuananhhcmus@gmail.com>
2014-10-28 16:23:05 +0700

    add constant class

---
+++ src/test/java/util/Constant.java
@@ -0,0 +1,13 @@
+package util;
+
+/**
+ * Created by anhpham on 28/10/2014.
+ */
+public  final class Constant {
+
+    public static final String SERVER_ADDRESS = "http://127.0.0.1:4723/wd/hub";
+    public static final String APP_PACKAGE = "com.lazada.android";
+    public static final String LOCAL_APP = "lazada-android.apk";
+
+
+}
