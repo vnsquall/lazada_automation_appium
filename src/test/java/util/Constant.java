97523ca2a5a5e8958c9ed4f00ab00fc9d5cead67
hamaianhnhi <tuananhhcmus@gmail.com>
2014-10-28 16:40:17 +0700

    add variables in Constant.java

--- src/test/java/util/Constant.java
+++ src/test/java/util/Constant.java
@@ -8,6 +8,14 @@ public  final class Constant {
     public static final String SERVER_ADDRESS = "http://127.0.0.1:4723/wd/hub";
     public static final String APP_PACKAGE = "com.lazada.android";
     public static final String LOCAL_APP = "lazada-android.apk";
+    public static final String AUTOMATION_NAME = "Appium";
+    public static final String APPIUM_VERSION = "1.2.3";
+    public static final String PLATFORM_NAME = "Android";
+    public static final String PLATFORM_VERSION = "4.4";
+    public static final String DEVICE_NAME = "Android Emulator";
+    public static final String APP_ACTIVITY = "pt.rocket.view.ChangeCountryFragmentActivity";
+    public static final String SCREENSHOT_PATH = "Path/here";
+
 
 
 }
