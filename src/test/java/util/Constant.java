8c2604a6d7accbb5539ba8167fa864a478365fda
hamaianhnhi <tuananhhcmus@gmail.com>
2014-10-29 16:16:56 +0700

    add BROWSER_NAME in Constant.java

--- src/test/java/util/Constant.java
+++ src/test/java/util/Constant.java
@@ -11,6 +11,7 @@ public  final class Constant {
     public static final String LOCAL_APP_LIVE = "lazada-android.apk";
     public static final String LOCAL_APP_STAGING = "lazada-android-dev.apk";
     public static final String AUTOMATION_NAME = "Appium";
+    public static final String BROWSER_NAME = "";
     public static final String APPIUM_VERSION = "1.2.3";
     public static final String PLATFORM_NAME = "Android";
     public static final String PLATFORM_VERSION = "4.4";
@@ -18,8 +19,5 @@ public  final class Constant {
     public static final String APP_ACTIVITY = "pt.rocket.view.ChangeCountryFragmentActivity";
     public static final String SCREENSHOT_PATH = "Path/here";
     public static final int TIMEOUT = 30;
-
-
-
 
 }
