7afbf6b8ebe4e66a10c5e446c780402a66d9b0cc
hamaianhnhi <tuananhhcmus@gmail.com>
2014-11-17 17:13:10 +0700

    add class Login_Screen.java

---
+++ src/test/java/screenObjects/android_app/Login_Screen.java
@@ -0,0 +1,34 @@
+package screenObjects.android_app;
+
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static util.Helper.*;
+
+/**
+ * Created by anhpham on 17/11/2014.
+ */
+public class Login_Screen {
+
+    private static String proceedToCheckout = "resourceID::checkout_button";
+    private static String delete = "resourceID::delete_button";
+    private static String changeQuantity = "resourceID::hangequantity_button";
+
+    public static void click_ProceedToCheckoutBtn() {
+
+        findByUISelector(split(proceedToCheckout)[0], split(proceedToCheckout)[1]).click();
+    }
+
+    public static List<WebElement> delete () {
+
+        return findsByUISelector(split(delete)[0], split(delete)[1]);
+    }
+
+    public static List<WebElement> changeQuantity () {
+
+        return findsByUISelector(split(changeQuantity)[0], split(changeQuantity)[1]);
+    }
+
+
+}
