3361509fede65deb7bd2a2b11faa874f9f87fcbf
hamaianhnhi <tuananhhcmus@gmail.com>
2014-11-17 15:48:19 +0700

    add functions: click_CreditCardRadio, input_CreditName in PayMethod_Screen.java

--- src/test/java/screenObjects/android_app/PayMethod_Screen.java
+++ src/test/java/screenObjects/android_app/PayMethod_Screen.java
@@ -1,5 +1,7 @@
 package screenObjects.android_app;
 
+import org.openqa.selenium.WebElement;
+
 import static util.Helper.findByUISelector;
 import static util.Helper.split;
 
@@ -10,7 +12,14 @@ public class PayMethod_Screen {
 
     public static String radioCODDisabled = "xpath:://*[@class='payment-method-option radio'and@value='CashOnDelivery'and@disabled='disabled']";
     public static String labelCOD = "xpath:://label[@for='cashondelivery']";
+    public static String labelCreditCard = "xpath:://label[@class='creditcards']";
     public static String continueButton = "xpath:://button[@class='orange-button']";
+    public static String creditNumber = "xpath:://*[@id='PaymentMethodForm_parameter_cc_number']";
+    public static String creditCustomerName = "xpath:://*[@id='orange-PaymentMethodForm_parameter_cc_holder']";
+    public static String creditSecurityCode = "xpath:://*[@id='PaymentMethodForm_parameter_cc_security_code']";
+    public static String creditMonth = "xpath:://*[@id='PaymentMethodForm_parameter_cc_exp_month']";
+    public static String creditYear = "xpath:://*[@id='PaymentMethodForm_parameter_cc_exp_year']";
+    public static String continue_ = "xpath:://*[@id='PaymentMethodForm_parameter_cc_exp_year']";
 
 
     public static void click_CashOnDeliveryRadio() {
@@ -22,5 +31,39 @@ public class PayMethod_Screen {
 
         findByUISelector(split(continueButton)[0], split(continueButton)[1]).click();
     }
+
+    public static void click_CreditCardRadio() {
+
+        findByUISelector(split(labelCreditCard)[0], split(labelCreditCard)[1]).click();
+    }
+
+    public static void input_CreditNumber(String creditNumber) {
+
+        findByUISelector(split(PayMethod_Screen.creditNumber)[0], split(PayMethod_Screen.creditNumber)[1]).click();
+        findByUISelector(split(PayMethod_Screen.creditNumber)[0], split(PayMethod_Screen.creditNumber)[1]).sendKeys(creditNumber);
+    }
+
+    public static void input_CreditName(String customerName) {
+
+        findByUISelector(split(creditCustomerName)[0], split(creditCustomerName)[1]).click();
+        findByUISelector(split(creditCustomerName)[0], split(creditCustomerName)[1]).sendKeys(customerName);
+    }
+
+    public static void input_CreditSecurityCode(String securityCode) {
+
+        findByUISelector(split(creditSecurityCode)[0], split(creditSecurityCode)[1]).click();
+        findByUISelector(split(creditSecurityCode)[0], split(creditSecurityCode)[1]).sendKeys(securityCode);
+    }
+
+    public static WebElement creditCardMonthCbx () {
+
+        return findByUISelector(split(creditMonth)[0], split(creditMonth)[1]);
+    }
+
+    public static WebElement creditCardYearCbx () {
+
+        return findByUISelector(split(creditYear)[0], split(creditYear)[1]);
+    }
+
 
 }
