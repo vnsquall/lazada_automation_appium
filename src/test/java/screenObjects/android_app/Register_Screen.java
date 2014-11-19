2f0c979c4270847d9a979d294f74ad5ab995414e
hamaianhnhi <tuananhhcmus@gmail.com>
2014-11-19 16:36:21 +0700

    add class Register_Screen.java

---
+++ src/test/java/screenObjects/android_app/Register_Screen.java
@@ -0,0 +1,46 @@
+package screenObjects.android_app;
+
+import static util.Helper.*;
+
+/**
+ * Created by anhpham on 17/11/2014.
+ */
+public class Register_Screen {
+
+    public static String textBoxes = "xpath:://*[contains(@class,'android.widget.EditText')]";
+    public static String showPassword = "class::android.widget.CheckBox";
+    public static String submit = "resourceID::register_button_submit";
+
+    public static void input_Email (String email) {
+
+        findsByUISelector(split(textBoxes)[0], split(textBoxes)[1]).get(0).sendKeys(email);
+    }
+
+    public static void input_Password (String password) {
+
+        findsByUISelector(split(textBoxes)[0], split(textBoxes)[1]).get(1).sendKeys(password);
+    }
+
+    public static void input_RePassword (String password) {
+
+        findsByUISelector(split(textBoxes)[0], split(textBoxes)[1]).get(2).sendKeys(password);
+    }
+
+    public static void input_Name (String name) {
+
+        findsByUISelector(split(textBoxes)[0], split(textBoxes)[1]).get(3).sendKeys(name);
+    }
+
+    public static void click_ShowPassword () {
+
+        findByUISelector(split(showPassword)[0], split(showPassword)[1]).click();
+    }
+
+    public static void click_SubmitBtn () {
+
+        findByUISelector(split(submit)[0], split(submit)[1]).click();
+    }
+
+
+
+}
