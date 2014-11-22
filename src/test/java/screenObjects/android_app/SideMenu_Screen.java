e1cd01d199aeb6a0f6edb71c3b6279db46b4fe97
hamaianhnhi <tuananhhcmus@gmail.com>
2014-11-22 11:11:14 +0700

    add class SideMenu_Screen.java

---
+++ src/test/java/screenObjects/android_app/SideMenu_Screen.java
@@ -0,0 +1,23 @@
+package screenObjects.android_app;
+
+
+
+import static util.Helper.*;
+
+/**
+ * Created by anhpham on 17/11/2014.
+ */
+public class SideMenu_Screen {
+
+    public static void click_Menu(String menuName) {
+
+        find_TextView_Android(menuName).click();
+    }
+
+
+
+
+
+
+
+}
