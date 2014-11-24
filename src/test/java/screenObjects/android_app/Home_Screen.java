39ec21112a43fca960b710f0fb74601a3ea33443
hamaianhnhi <tuananhhcmus@gmail.com>
2014-11-24 17:22:12 +0700

    add function: input_search in Home_Screen.java

--- src/test/java/screenObjects/android_app/Home_Screen.java
+++ src/test/java/screenObjects/android_app/Home_Screen.java
@@ -8,10 +8,16 @@ import static util.Helper.*;
 public class Home_Screen {
 
     public static String product_features_title = "resourceID::product_features_title";
+    public static String search = "resourceID::search_component";
 
     public static void loaded() {
 
         findElement(split(product_features_title)[0], split(product_features_title)[1]);
+    }
+
+    public static void input_search(String query) {
+
+        findElement(split(search)[0], split(search)[1]).sendKeys(query);
     }
 
 
