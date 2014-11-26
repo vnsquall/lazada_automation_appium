34be315ceb0c3cdee121a7005d071a7996b5ac5b
hamaianhnhi <tuananhhcmus@gmail.com>
2014-11-26 08:45:54 +0700

    add function: click_SignOutConfirmBtn in SideMenu_Screen.java

--- src/test/java/screenObjects/android_app/SideMenu_Screen.java
+++ src/test/java/screenObjects/android_app/SideMenu_Screen.java
@@ -9,15 +9,17 @@ import static util.Helper.*;
  */
 public class SideMenu_Screen {
 
+    private static String signOutConfirm = "resourceID::button2";
+
     public static void click_Menu(String menuName) {
 
         find_TextView_Android(menuName).click();
     }
 
+    public static void click_SignOutConfirmBtn() {
 
-
-
-
+        findElement(split(signOutConfirm)[0], split(signOutConfirm)[1]).click();
+    }
 
 
 }
