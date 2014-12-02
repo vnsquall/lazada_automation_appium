60f57deb2781eeeb9224ee9e16bbc7cbd561b67c
hamaianhnhi <tuananhhcmus@gmail.com>
2014-12-02 16:12:52 +0700

    add function: getRichRelevanceProducts in Home_Screen.java

--- src/test/java/screenObjects/android_app/Home_Screen.java
+++ src/test/java/screenObjects/android_app/Home_Screen.java
@@ -1,5 +1,11 @@
 package screenObjects.android_app;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static util.Helper.*;
 
 /**
@@ -9,6 +15,8 @@ public class Home_Screen {
 
     public static String product_features_title = "resourceID::product_features_title";
     public static String search = "resourceID::search_component";
+    public static String groupTeaser = "resourceID::teaser_group_container";
+    public static String productImg = "com.lazada.android:id/image_view"; // Special case: Products in group teaser
 
     public static void loaded() {
 
@@ -18,6 +26,29 @@ public class Home_Screen {
     public static void input_search(String query) {
 
         findElement(split(search)[0], split(search)[1]).sendKeys(query);
+    }
+
+    /**
+     * Find array of products in Rich relevance
+     */
+    public static List<WebElement> getRichRelevanceProducts () {
+
+        List<WebElement> products = new ArrayList<WebElement>();
+        List<WebElement> groupTeasers = findElements(split(groupTeaser)[0], split(groupTeaser)[1]);
+        System.out.println("number of group teaser >>>> " + groupTeasers.size());
+
+        for (WebElement groupTeaser : groupTeasers) {
+
+            List<WebElement> _products = groupTeaser.findElements(By.id(productImg));
+            System.out.println("number of product >>>" + _products.size());
+
+            for (WebElement product : _products) {
+                System.out.println("name >>>>>>" + product.getText());
+                products.add(product);
+            }
+        }
+
+        return products;
     }
 
 
