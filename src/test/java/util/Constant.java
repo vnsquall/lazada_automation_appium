2d9d98751baa0255ae0301b687851dfb44638530
hamaianhnhi <tuananhhcmus@gmail.com>
2014-12-12 09:51:04 +0700

    changes APP_ACTIVITY in Constant.java

--- src/test/java/util/Constant.java
+++ src/test/java/util/Constant.java
@@ -8,7 +8,7 @@ public  final class Constant {
     public static final String SERVER_ADDRESS = "http://127.0.0.1:4723/wd/hub";
     public static final String APP_PACKAGE_LIVE = "com.lazada.android";
     public static final String APP_PACKAGE_STAGING = "pt.rocket.lazada.dev";
-    public static final String LOCAL_APP_LIVE = "lazada-android.apk";
+    public static final String LOCAL_APP_LIVE = "Lazada_v2.8.1_67_Mobogenie.apk";
     public static final String LOCAL_APP_STAGING = "lazada-android-dev.apk";
     public static final String AUTOMATION_NAME = "Appium";
     public static final String BROWSER_NAME = "";
@@ -16,7 +16,7 @@ public  final class Constant {
     public static final String PLATFORM_NAME = "Android";
     public static final String PLATFORM_VERSION = "4.4";
     public static final String DEVICE_NAME = "Android Emulator";
-    public static final String APP_ACTIVITY = "pt.rocket.view.ChangeCountryFragmentActivity";
+    public static final String APP_ACTIVITY = ""; // pt.rocket.view.ChangeCountryFragmentActivity
     public static final String SCREENSHOT_PATH = "Path/here";
     public static final String USERNAME = "amili@mail.com";
     public static final String PASSWORD = "a12345";
