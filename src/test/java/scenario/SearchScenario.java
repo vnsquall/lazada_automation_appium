9408e51717bfd5823d081982e80d4d0c113e09a9
hamaianhnhi <tuananhhcmus@gmail.com>
2014-12-16 16:55:58 +0700

    add function: searchPopular in SearchScenario.java

--- src/test/java/scenario/SearchScenario.java
+++ src/test/java/scenario/SearchScenario.java
@@ -42,4 +42,9 @@ public class SearchScenario extends AppiumSetupTest {
             homeSort.click();
         }
     }
+
+    protected void searchPopular () {
+
+        Home_Screen.click_SearchTxt();
+    }
 }
